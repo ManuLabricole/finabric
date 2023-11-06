b48cf6a69c59aae89d93a8e23f75cdf3da059537
Manu Labricole <manu_barriol@hotmail.fr>
2023-11-06 23:20:34 +0100

    create custom registration view

--- backend/user_profile/views.py
+++ backend/user_profile/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import (
+    CustomUserRegistrationSerializer,
+)  # assuming you created a custom serializer
 
-# Create your views here.
+
+class CustomRegistrationView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = CustomUserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                return Response(
+                    {"message": "User created successfully."},
+                    status=status.HTTP_201_CREATED,
+                )
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
