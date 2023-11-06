818cfa58daea05a405572c0e9fe87483e3c7b73f
Manu Labricole <manu_barriol@hotmail.fr>
2023-11-06 22:09:45 +0100

    add minimum to password validation

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,6 +32,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    os.getenv("FRONTEND_HOST"),
+]
 
 # Application definition
 
@@ -94,13 +97,16 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
         "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {
+            "min_length": 8,
+        },
     },
 ]
 
