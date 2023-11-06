281534e868e83873c3c681969c933620709409a3
Manu Labricole <manu_barriol@hotmail.fr>
2023-11-06 23:55:37 +0100

    adjust the route to the new view name

--- backend/user_profile/urls.py
+++ backend/user_profile/urls.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 
-from user_profile.views import CustomRegistrationView
+from user_profile.views import UserRegistrationAPIView
 
 
 urlpatterns = [
     path(
         "register/",
-        CustomRegistrationView.as_view(),
+        UserRegistrationAPIView.as_view(),
         name="user-profile-register",
     ),
 ]
