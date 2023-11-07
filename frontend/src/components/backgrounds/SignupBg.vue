f674d9a7c0bf6126df04655a824e4014480f1f24
Manu Labricole <manu_barriol@hotmail.fr>
2023-11-08 00:27:28 +0100

    m

--- frontend/src/components/backgrounds/SignupBg.vue
+++ frontend/src/components/backgrounds/SignupBg.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="bg-grey h"></div>
+  <div class="input-container">
+    <input
+      v-model="inputValue"
+      placeholder="Email"
+      @focus="isFocused = true"
+      @blur="isFocused = false"
+      class="border-b-2 border-red-300 focus:border-red-500 p-2 text-base outline-none transition-all duration-300"
+    />
+    <label :class="{ 'text-sm transform translate-y-[-1.5rem] translate-x-[-0.5rem] text-red-500': isFocused }">Your Label</label>
+  </div>
 </template>
 
 <script>
