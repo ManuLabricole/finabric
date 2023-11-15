7130487717beaaa87a8c302fad669328b33a23de
Manu Labricole <manu_barriol@hotmail.fr>
2023-11-16 00:14:39 +0100

    style toast 1

--- frontend/src/components/common/ToastInfo.vue
+++ frontend/src/components/common/ToastInfo.vue
@@ -2,11 +2,11 @@
   <div
     class="flex items-center justify-center rounded-full bg-success border-2 border-green-700 p-2"
   >
-    <span class="text-3xl mr-2">🎉</span>
+    <span class="emoji_left text-3xl -rotate-90">🎉</span>
     <!-- Increase emoji size with text-3xl -->
-    <div class="mx-2">Félicitations ! Ton compte a été créé</div>
+    <div class="mx-6">Félicitations ! Ton compte a été créé</div>
     <!-- Add some margin for spacing -->
-    <span class="text-3xl ml-2">🎉</span>
+    <span class="emoji_right text-3xl">🎉</span>
     <!-- Increase emoji size with text-3xl -->
   </div>
 </template>
@@ -22,4 +22,31 @@ export default {
 }
 </script>
 
-<style postcss scoped></style>
+<style postcss scoped>
+@keyframes rotateAndScaleRight {
+  0%,
+  100% {
+    transform: rotate(-10deg) scale(1);
+  }
+  50% {
+    transform: rotate(10deg) scale(1.1);
+  }
+}
+
+@keyframes rotateAndScaleLeft {
+  0%,
+  100% {
+    transform: rotate(-100deg) scale(1);
+  }
+  50% {
+    transform: rotate(-80deg) scale(1.1);
+  }
+}
+
+.emoji_right {
+  animation: rotateAndScaleRight 0.5s ease-in-out 3; /* duration, timing function, iteration count */
+}
+.emoji_left {
+  animation: rotateAndScaleLeft 0.5s ease-in-out 3; /* duration, timing function, iteration count */
+}
+</style>
