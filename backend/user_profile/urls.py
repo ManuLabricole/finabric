3435730dc1df3284702befa094991d35fac8e934
Manu Labricole <manu_barriol@hotmail.fr>
2023-11-16 22:54:07 +0100

    add routes for jsonWT

--- backend/user_profile/urls.py
+++ backend/user_profile/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from django.urls.conf import include
 
 from user_profile.views import UserRegistrationAPIView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
@@ -10,5 +11,15 @@ urlpatterns = [
         "auth/register/",
         UserRegistrationAPIView.as_view(),
         name="register",
+    ),
+    path(
+        "auth/login/",
+        TokenObtainPairView.as_view(),
+        name="login",
+    ),
+    path(
+        "auth/token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh",
     ),
 ]
