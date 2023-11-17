7ce2228c8ab6ceddc90f492cf2ac50ef4551f0d1
Manu Labricole <manu_barriol@hotmail.fr>
2023-11-17 23:59:46 +0100

    update fields

--- backend/user_account/models.py
+++ backend/user_account/models.py
@@ -8,6 +8,8 @@ from django.contrib.auth.models import (
 )
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
+from django.core.validators import MinValueValidator
+from PIL import Image
 
 
 class CustomUserManager(BaseUserManager):
@@ -64,12 +66,6 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     )
 
 
-from django.db import models
-from django.utils.translation import gettext_lazy as _
-from django.core.validators import MinValueValidator
-from PIL import Image
-
-
 class UserProfile(models.Model):
     # Link to the CustomUser model
     user = models.OneToOneField(
@@ -78,14 +74,24 @@ class UserProfile(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     # Additional fields
     avatar = models.ImageField(
-        upload_to="avatars/", default="avatars/default.jpg", blank=True
+        upload_to="avatars/", default="avatars/default.png", blank=True
     )  # This indicate a subdirectory of the MEDIA_ROOT directory to store the uploaded file
     first_name = models.CharField(
-        _("first name"), max_length=150, blank=True, null=True
+        _("first name"), max_length=150, blank=True, null=True, editable=True
     )
-    last_name = models.CharField(_("last name"), max_length=150, blank=True, null=True)
+    last_name = models.CharField(
+        _("last name"),
+        max_length=150,
+        blank=True,
+        null=True,
+        editable=True,
+    )
     age = models.PositiveIntegerField(
-        default=18, validators=[MinValueValidator(18)], blank=True, null=True
+        default=18,
+        validators=[MinValueValidator(18)],
+        blank=True,
+        null=True,
+        editable=True,
     )
     monthly_income = models.DecimalField(
         max_digits=10,
@@ -93,6 +99,7 @@ class UserProfile(models.Model):
         validators=[MinValueValidator(0)],
         blank=True,
         null=True,
+        editable=True,
     )
     monthly_expenses = models.DecimalField(
         max_digits=10,
@@ -100,9 +107,10 @@ class UserProfile(models.Model):
         validators=[MinValueValidator(0)],
         blank=True,
         null=True,
+        editable=True,
     )
-    saving_profile = models.TextField(blank=True)
-    country = models.CharField(max_length=50, blank=True)
+    saving_profile = models.TextField(blank=True, editable=True)
+    country = models.CharField(max_length=50, blank=True, editable=True)
 
     def __str__(self):
         return f"{self.user.email}"
