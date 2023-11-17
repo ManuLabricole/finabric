d65de435bcdb9142c00ddb3aa3b056adfa99221f
Manu Labricole <manu_barriol@hotmail.fr>
2023-11-17 22:31:11 +0100

    validate test of email normalization

--- backend/user_account/models.py
+++ backend/user_account/models.py
@@ -14,7 +14,7 @@ class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError("Users must have an email address")
-        email = self.normalize_email(email)
+        email = email.lower()
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
--- backend/user_account/tests.py
+++ backend/user_account/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
+from django.db import IntegrityError
 
-from django.test import TestCase
 from .models import CustomUser
 
 
@@ -24,12 +24,43 @@ class CustomUserModelTest(TestCase):
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
 
+    def test_email_normalization(self):
+        user = CustomUser.objects.create_user(
+            email="TestEmail@Example.Com", password="testpass123"
+        )
+        print(user.email)
+        self.assertEqual(user.email, "testemail@example.com")
 
+    def test_user_string_representation(self):
+        user = CustomUser.objects.create_user(
+            email="user@example.com", password="testpass123"
+        )
+        self.assertEqual(str(user), user.email)
+
+    def test_user_password_change(self):
+        user = CustomUser.objects.create_user(
+            email="user@example.com", password="old_password"
+        )
+        user.set_password("new_password")
+        self.assertTrue(user.check_password("new_password"))
+
+    def test_duplicate_email(self):
+        CustomUser.objects.create_user(email="user@example.com", password="testpass123")
+        with self.assertRaises(IntegrityError):
+            CustomUser.objects.create_user(
+                email="user@example.com", password="testpass456"
+            )
+
+    def test_required_fields(self):
+        user = CustomUser.objects.create_user(
+            email="user@example.com", password="testpass123"
+        )
+        self.assertIsNotNone(user.email)
 
 
 class PasswordValidatorTest(TestCase):
     def test_special_character_validator(self):
         user = CustomUser(email="testuser")
         with self.assertRaises(ValidationError):
-            user.set_password("passwordWithoutSpecialChar")
+            user.set_password("ADino1546zedSD")
             user.full_clean()
